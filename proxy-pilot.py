67f10009ab3e6a2eaa131acb66473536d6a56cbd
Chet Brennen <chet.brennen@gmail.com>
2012-05-02 12:25:53 -0500

    master script file

---
+++ proxy-pilot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Poor mans Bluecoat director by: Chet Brennen
+# Version 0.1 (May 02 2012)
+
+# Contact me at chet.brennen@gmail.com
+
+    print ""
+    print ""
+    print "________________________________________________"
+    print "Proxy-Pilot"
+    print "by Chet Brennen (chet.brennen@gmail.com)"
+    print ""
+    print "Version 0.1 (May 02 2012)     ^__^"
+    print "                              (oo)\________"
+    print "                              (__)\        )\/\ "
+    print "                                  ||----w |"
+    print "Power to teh cows!                ||     ||"
+    print "________________________________________________"
+    print ""
+ #eof   
