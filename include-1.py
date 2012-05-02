697a59b9dbfd1723bcc0c966cc2c2468221b4f9c
Chet Brennen <chet.brennen@gmail.com>
2012-05-02 13:37:43 -0500

    updated by JC

--- include-1.py
+++ include-1.py
@@ -1,1 +1,8 @@
-Hey Ho here we go!!!!!
+#!/usr/bin/python
+
+# Poor mans Bluecoat director by: Chet Brennen
+# Version 0.1 (May 02 2012)
+
+# Contact me at chet.brennen@gmail.com
+
+include '_version'
