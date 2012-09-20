b91af9bbf9a4a74d81034e07fb548f21b0e355fe
chetbrennen <chet.brennen@gmail.com>
2012-09-20 10:56:38 -0500

    Update include-1.py

--- include-1.py
+++ include-1.py
@@ -10,4 +10,7 @@ include '_version'
 The cow says MOOOOOOOOOO
 The duck says QUAAAAACK
 The sheep says BAAAAAAAHHHHHHH
-The goat says Fuck You Sheep!!!
+The snake says SSSSSSSSSSSSSSSSSssssssssssssssssss
+The bear says GRRRRRRRRRRRRRRR
+The ferret says fuck you bear .... fuck you!
+The bear says dude, chill .... take it easy little man   . I'm just a bear thats how I sound
